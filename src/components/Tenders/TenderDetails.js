32c052e58a1355d1080065e7f7ccbc313815e6be
meetvelani <meetvelani2728@gmail.com>
2024-07-16 08:36:42 +0530

    added lock in tender more information

--- src/components/Tenders/TenderDetails.js
+++ src/components/Tenders/TenderDetails.js
@@ -252,11 +252,18 @@ export default function TenderDetails(props) {
                 <div
                     data-bs-toggle='collapse'
                     href='#otherInformation'
-                    className='collapsed question'
+                    className={`question ${tenders_data?.authority_name ? '' : 'cursor-not-allowed'}`}
                 >
                     Other Information
                     <i className='bi bi-chevron-down icon-show'></i>
                     <i className='bi bi-chevron-up icon-close'></i>
+                    {
+                        tenders_data?.authority_name
+                            ?
+                           ''
+                            :
+                            <i className='bi bi-lock-fill locIcons'></i>
+                    }
                 </div>
                 <div
                     id='otherInformation'
