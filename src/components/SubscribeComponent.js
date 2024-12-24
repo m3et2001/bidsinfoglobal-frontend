1ae28074752f76e5b9d1d7b6d7e978ea871c9c69
m3et2001 <meetvelani2728@gmail.com>
2024-12-24 15:44:49 +0530

    fix issue

--- src/components/SubscribeComponent.js
+++ src/components/SubscribeComponent.js
@@ -5,59 +5,84 @@ import { isAuth } from "../helpers/cookies";
 import { Dialog } from "primereact/dialog";
 import CallToActionRegisterComponent from "./CallToActionRegisterComponent";
 
-export default function SubscribeComponent({ data, handlePlan, submit_loading, success }) {
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const [submitLoading, setSubmitLoading] = useState(false);
-    const [visible, setVisible] = useState(false);
+export default function SubscribeComponent({
+  data,
+  handlePlan,
+  submit_loading,
+  success,
+}) {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [submitLoading, setSubmitLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const openDialog = () => setVisible(true);
-    const closeDialog = () => setVisible(false);
-    const onCategoryChange = (e) => {
-        let _selectedCategories = [...selectedCategories];
+  const openDialog = () => setVisible(true);
+  const closeDialog = () => setVisible(false);
+  const onCategoryChange = (e) => {
+    let _selectedCategories = [...selectedCategories];
 
-        if (e.checked) _selectedCategories.push(e.value);
-        else
-            _selectedCategories = _selectedCategories.filter(
-                (category) => category.value !== e.value
-            );
+    if (e.checked) _selectedCategories.push(e.value);
+    else
+      _selectedCategories = _selectedCategories.filter(
+        (category) => category.value !== e.value
+      );
 
-        setSelectedCategories(_selectedCategories);
-    };
-    const handlePlanSelect = (key, val, selectedCategories) => {
-        if (
-            isAuth()
-        ) {
-            setSubmitLoading(key)
-            handlePlan({ plan_id: val.plan_id, categories: selectedCategories })
-        }
-        else {
-            openDialog()
-        }
+    setSelectedCategories(_selectedCategories);
+  };
+  const handlePlanSelect = (key, val, selectedCategories) => {
+    if (isAuth()) {
+      setSubmitLoading(key);
+      handlePlan({ plan_id: val.plan_id, categories: selectedCategories ,formData:formData});
+    } else {
+      openDialog();
     }
+  };
+  const selectTagStyle = {
+    fontSize: "14px",
+    paddingBottom: "5px",
+    height: "34px",
+    width: "100%",
+    border: "1px solid #dee2e6",
+    padding: ".375rem .75rem",
+    borderRadius: ".375rem",
+  };
+  const [formData, setFormData] = useState({
+    dataType: "xml",
+    dataDeliveryType: "api",
+    dataCategory: "multi_industry",
+    emailAlerts: 1,
+    userId: 1,
+  });
 
-    return (
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-        <div className="row justify-content-center">
-            {
-                success ?
-                    <div className="alert alert-success">
-                        {success.message}
-                    </div>
-                    :
-                    null
-            }
-            {
-                data.map(function (val, key) {
-                    return (
-                        <div
-                            className="col-lg-4 col-md-6"
-                            data-aos="fade-up"
-                            data-aos-delay="100"
-                            key={key}
-                        >
-                            <div className="box" >
-                                <h3>{val?.title}</h3>
-                                {/* {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data:", formData);
+    // You can send this data to an API or process it further.
+  };
+
+  return (
+    <div className="row justify-content-center">
+      {success ? (
+        <div className="alert alert-success">{success.message}</div>
+      ) : null}
+      {data.map(function (val, key) {
+        return (
+          <div
+            className="col-lg-4 col-md-6"
+            data-aos="fade-up"
+            data-aos-delay="100"
+            key={key}
+          >
+            <div className="box">
+              <h3>{val?.title}</h3>
+              {/* {
                                     val.amount > 0
                                         ?
                                         <h4>
@@ -67,69 +92,164 @@ export default function SubscribeComponent({ data, handlePlan, submit_loading, s
                                         null
                                 } */}
 
-                                {
-                                    val.plan_name === "CUSTOMISE_PACKAGE"
-                                        ?
-                                        <ul className="checboixpackage">
-                                            {
-                                                val?.access && val?.access?.map(function (dd, key) {
-                                                    return (
-                                                        <li
-                                                            key={key}
-                                                            className="flex align-items-center"
-                                                        >
-                                                            <Checkbox
-                                                                inputId={key}
-                                                                name="category"
-                                                                value={dd}
-                                                                onChange={onCategoryChange}
-                                                                checked={selectedCategories.some(
-                                                                    (item) => item === dd
-                                                                )}
-                                                            />
-                                                            <label htmlFor={key} className="ml-2">
-                                                                {dd}
-                                                            </label>
-                                                        </li>
-                                                    )
-                                                })
-                                            }
-                                        </ul>
-                                        :
-                                        <ul>
-                                            {
-                                                val?.access?.map(function (dd, key) {
-                                                    return (
-                                                        <li key={key}>{dd}</li>
-                                                    )
-                                                })
-                                            }
-                                        </ul>
-                                }
+              {val.plan_name === "CUSTOMISE_PACKAGE" ? (
+                <ul className="checboixpackage">
+                  {val?.access &&
+                    val?.access?.map(function (dd, key) {
+                      return (
+                        <li key={key} className="flex align-items-center">
+                          <label htmlFor={key} className="ml-2">
+                            {dd}
+                          </label>
+                        </li>
+                      );
+                    })}
+                  <li>
+                    <div className="input-container form-group">
+                      <label
+                        htmlFor="dataType"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                      >
+                        Data Type
+                      </label>
+                      <select
+                        id="dataType"
+                        name="dataType"
+                        style={selectTagStyle}
+                        value={formData.dataType}
+                        onChange={handleChange}
+                      >
+                        <option value="" disabled>
+                          -- Select an option --
+                        </option>
+                        <option value="xml">XML</option>
+                        <option value="newsletter">Newsletter</option>
+                        <option value="json">JSON</option>
+                        <option value="csv">CSV</option>
+                      </select>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="input-container form-group">
+                      <label
+                        htmlFor="dataDeliveryType"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                      >
+                        Data Delivery Type
+                      </label>
+                      <select
+                        id="dataDeliveryType"
+                        name="dataDeliveryType"
+                        style={selectTagStyle}
+                        value={formData.dataDeliveryType}
+                        onChange={handleChange}
+                      >
+                        <option value="" disabled>
+                          -- Select an option --
+                        </option>
+                        <option value="api">API</option>
+                        <option value="micro_site">Micro-site</option>
+                      </select>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="input-container form-group">
+                      <label
+                        htmlFor="dataCategory"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                      >
+                        Data Category
+                      </label>
+                      <select
+                        id="dataCategory"
+                        name="dataCategory"
+                        style={selectTagStyle}
+                        value={formData.dataCategory}
+                        onChange={handleChange}
+                      >
+                        <option value="" disabled>
+                          -- Select an option --
+                        </option>
+                        <option value="multi_industry">Multi Industry</option>
+                        <option value="bulk_data">Bulk data</option>
+                        <option value="republishing_rights">
+                          Republishing rights
+                        </option>
+                      </select>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="input-container form-group">
+                      <label
+                        htmlFor="emailAlerts"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                      >
+                        No. Email Alerts
+                      </label>
+                      <input
+                        id="emailAlerts"
+                        name="emailAlerts"
+                        type="number"
+                        placeholder="Enter no. email alerts"
+                        className="form-control"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                        value={formData.emailAlerts}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </li>
+                  <li>
+                    <div className="input-container form-group">
+                      <label
+                        htmlFor="userId"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                      >
+                        No. User Id
+                      </label>
+                      <input
+                        id="userId"
+                        name="userId"
+                        type="number"
+                        placeholder="Enter no. user id"
+                        className="form-control"
+                        style={{ fontSize: "14px", paddingBottom: "5px" }}
+                        value={formData.userId}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </li>
+                </ul>
+              ) : (
+                <ul>
+                  {val?.access?.map(function (dd, key) {
+                    return <li key={key}>{dd}</li>;
+                  })}
+                </ul>
+              )}
 
-                                <div className="btn-wrap">
-                                    <Button value={"Choose Plan"} loading={submit_loading && submitLoading === key} className="btn-buy" onClick={() => handlePlanSelect(key, val, selectedCategories)} />
-                                </div>
-                            </div>
+              <div className="btn-wrap">
+                <Button
+                  value={"Choose Plan"}
+                  loading={submit_loading && submitLoading === key}
+                  className="btn-buy"
+                  onClick={() => handlePlanSelect(key, val, selectedCategories)}
+                />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <Dialog
+        className="ModalMaindiwo "
+        header={"Join Us Today for Exclusive Benefits!"}
+        visible={visible}
+        style={{ maxWidth: "80vw", textAlign: "left" }}
+        onHide={closeDialog}
+      >
+        <CallToActionRegisterComponent closeDialog={closeDialog} />
+      </Dialog>
 
-                        </div>
-                    )
-                })
-            }
-            <Dialog
-                className='ModalMaindiwo '
-                header={"Join Us Today for Exclusive Benefits!"}
-                visible={visible}
-                style={{ maxWidth: "80vw", textAlign: "left" }}
-                onHide={closeDialog}
-            >
-                <CallToActionRegisterComponent closeDialog={closeDialog} />
-
-
-            </Dialog>
-
-
-            {/* <div
+      {/* <div
                 className="col-lg-4 col-md-6 mt-4 mt-lg-0"
                 data-aos="fade-up"
                 data-aos-delay="300"
@@ -169,6 +289,6 @@ export default function SubscribeComponent({ data, handlePlan, submit_loading, s
                     </div>
                 </div>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 }
